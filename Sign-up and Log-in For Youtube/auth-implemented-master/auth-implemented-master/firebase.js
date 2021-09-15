4b1d3b9a3d217efcbb9842415f9f5e6688042460
mohandatta <77618070+mohandatta@users.noreply.github.com>
2021-09-15 13:12:29 +0530

    Add files via upload

---
+++ Sign-up and Log-in For Youtube/auth-implemented-master/auth-implemented-master/firebase.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyCfHhDIruMTlKCGEOhdicKTIFyTaT_LPp8",
+    authDomain: "login-2cfe0.firebaseapp.com",
+    projectId: "login-2cfe0",
+    storageBucket: "login-2cfe0.appspot.com",
+    messagingSenderId: "747205814858",
+    appId: "1:747205814858:web:20ef2197d3994ddddf54b3",
+    measurementId: "G-3FR4L214M6"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
